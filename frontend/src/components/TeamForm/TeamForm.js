eb30c61d9db8869e802aeea8619519728b87d550
Ismaeel Abdulghani <ismaeelaa849@gmail.com>
2023-01-10 10:47:39 -0500

    Team form

---
+++ frontend/src/components/TeamForm/TeamForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Select from 'react-select'
+import {useState, useEffect} from 'react'
+import "./TeamForm.css"
+
+export default function TeamForm({characters,number, selected, setCharacter},) {
+    const [members, setMembers]=useState([])
+
+    const getData =  async () => {
+        const arr = [];
+          characters.map( (character) => {
+              arr.push({value: character.apiName, label: character.name})
+          });
+           setMembers(arr)
+
+      };
+    useEffect(() => {
+
+        getData();
+      }, [characters]);
+
+      function handleChange(val){
+        setCharacter(val)
+      }
+  return (
+    <div className="character-input">
+   
+        <Select value={selected} placeholder={"Character "+number} options={members} onChange={handleChange}/>
+
+        
+        </div>
+  )
+}
--- frontend/src/pages/Teams.js
+++ frontend/src/pages/Teams.js
@@ -1,5 +1,6 @@
 import  react, {useState, useEffect } from "react"
 import axios from "axios"
+import TeamForm from "../components/TeamForm/TeamForm"
 
 
 
@@ -8,6 +9,10 @@ const BASE_URL = "http://localhost:3001/teams"
 function Teams() {
     const [teams, setTeams] = useState([])
     const [characters, setCharacters] = useState([])
+    const [character1, setCharacter1] = useState("")
+    const [character2, setCharacter2] = useState("")
+    const [character3, setCharacter3] = useState("")
+    const [character4, setCharacter4] = useState("")
    
     
     useEffect(()=>{
@@ -31,14 +36,20 @@ function Teams() {
       }
 
       const addTeam = ()=>{
-        console.log(teams)
-        console.log(characters)
+        console.log(character1)
+        console.log(character2)
+        console.log(character3)
+        console.log(character4)
+        
       }
 
     return(
         <div className="App">
             <div className='team-input-wrapper'>
-
+                <TeamForm characters={characters}number={1} selected={character1} setCharacter={setCharacter1}/>
+                <TeamForm characters={characters}number={2} selected={character2} setCharacter={setCharacter2}/>
+                <TeamForm characters={characters}number={3} selected={character3} setCharacter={setCharacter3}/>
+                <TeamForm characters={characters}number={4} selected={character4} setCharacter={setCharacter4}/>
                 <button className ='add-button' onClick={addTeam} >Add</button>
         
             </div>
